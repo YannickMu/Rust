4dae9687616883c34d1a7d8a6e7a99a432a927d2
Yannick Mueller <info@yannickmueller.dev>
2024-06-01 08:48:25 +0200

    begin to build hashcracker in cargo folder

--- learn/cargo/src/main.rs
+++ learn/cargo/src/main.rs
@@ -1,34 +1,46 @@
-use druid::widget::{Label, Container, Flex, Split};
-use druid::{AppLauncher, Widget, WindowDesc};
-
-mod file;
-
-fn build_ui() -> impl Widget<()> {
-	Split::columns(
-		Container::new(
-			Flex::column()
-				.with_flex_child(Label::new("1. Kind"), 2.0)
-				.with_flex_child(Label::new("2. Kind"), 1.0),
-		),
-		Container::new(
-			Flex::row()
-				.with_flex_child(Label::new("1. Kind"), 2.0)
-				.with_flex_child(Label::new("2. Kind"), 1.0),
-		),
-	)
-	.solid_bar(true)
-	.draggable(true)
-	.split_point(0.3)
-	.bar_size(0.5)
-}
+use std::{io::stdout, io::Write, thread, sync::{Arc, Mutex}};
+use sha2::{Sha512, Digest};
+use rand::RngCore;
+use regex::Regex;
 
 fn main() {
-	let main_window = WindowDesc::new(build_ui())
-		.window_size((600.0, 400.0))
-		.title("Kontakte");
-	let initial_data = ();
-
-	AppLauncher::with_window(main_window)
-		.launch(initial_data)
-		.expect("Failed to launch application");
+	let mut count : u64 = 0;
+	let th = thread::spawn(move ||{
+		loop {
+			let hs = Regex::new(r"^AAAAAAAAAA").unwrap();
+			let mut data = [0u8; 128];
+			let mut hasher = Sha512::new();
+			rand::thread_rng().fill_bytes(&mut data);
+			hasher.update(data);
+			let res = format!("{:X}", hasher.finalize());
+			if hs.is_match(&res) {
+				println!("Found hash: {res:?}\nData: {data:?}");
+				std::process::exit(0);
+			}
+			count += 1;
+			write!(stdout().lock(), "Bytes tested: {}\r", count).unwrap();
+		}
+	});
+	let count = Arc::new(Mutex::new(0u64));
+	for _i in 1..11 {
+		let countse = Arc::clone(&count);
+		thread::spawn(move ||{
+			loop {
+				let hs = Regex::new(r"^AAAAAAAAAA").unwrap();
+				let mut data = [0u8; 128];
+				let mut hasher = Sha512::new();
+				rand::thread_rng().fill_bytes(&mut data);
+				hasher.update(data);
+				let res = format!("{:X}", hasher.finalize());
+				if hs.is_match(&res) {
+					println!("Found hash: {res:?}\nData: {data:?}");
+					std::process::exit(0);
+				}
+				let mut countse = countse.lock().unwrap();
+				*countse += 1;
+				write!(stdout().lock(), "Bytes tested: {}\r", *countse).unwrap();
+			}
+		});
+	}
+	th.join().unwrap();
 }
